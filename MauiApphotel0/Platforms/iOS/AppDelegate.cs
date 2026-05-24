c5d9862c85d586f18585a58ed8e5344934469bb1
User <User@DESKTOP-5EBDALV>
2026-05-24 18:27:36 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApphotel0/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiApphotel0
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
